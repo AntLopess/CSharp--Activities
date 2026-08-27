725358a2a2fc27a7d053261000e5ee091d114bde
AntLope <lusdf47@gmail.com>
2026-08-27 09:04:30 -0300

    Add files via upload

---
+++ Fundamentos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PersonalizandoMensagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*************************");
+
+            Console.WriteLine("Sistema - SENAI");
+
+            Console.WriteLine("Aluno: Lucas Lopes");
+
+            Console.WriteLine("Curso: Desenvolvimento de sistemas");
+
+            Console.WriteLine("Primeiro programa em C#");
+
+            Console.WriteLine("**************************");
+        }
+    }
+}
+
+
+
