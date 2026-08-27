0e176aa8a1fa6519ba8813ca05e654f343d409d6
AntLope <lusdf47@gmail.com>
2026-08-27 09:02:39 -0300

    Add files via upload

---
+++ Fundamentos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nome = "João Silva";
+
+        string curso = "Desenvolvimento de Sistemas";
+
+        string turma = "DS-01";
+
+        int ano = 2026;
+
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("Curso: " + curso);
+        Console.WriteLine("Turma: " + turma);
+        Console.WriteLine("ano: " + ano);
+    }
+}
