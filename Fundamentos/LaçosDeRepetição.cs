fadacd3dbc4d965aaac805a299dbc728b4c6630f
AntLope <lusdf47@gmail.com>
2026-08-27 10:15:30 -0300

    Add files via upload

---
+++ Fundamentos/LaçosDeRepetição.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LaçosDeRepetição
+{
+    class LaçosDeRepetição
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i<=5; i++) // Laço que começa em 1 e vai até 5.
+            {
+                Console.WriteLine("Contagem: " + i);// Exibe o valor de i a cada interação.
+            }
+            Console.ReadLine();
+        }
+    }
+}
