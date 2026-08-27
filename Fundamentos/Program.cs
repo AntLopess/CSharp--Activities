c8c482294d00ce606fe926685efed2e8cee379da
AntLope <lusdf47@gmail.com>
2026-08-27 09:25:19 -0300

    Add files via upload

---
+++ Fundamentos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Variaveis2
+{
+    static void Main(string[] args)
+    {
+        int idade = 25;
+        string nome = "João";
+        double altura = 1.75; // A variável 'altura' armazena um número decimal (tipo double).
+
+        Console.WriteLine("Nome: "+nome);
+        Console.WriteLine("Idade: "+idade);
+        Console.WriteLine("Altura: "+altura);
+        Console.ReadLine(); // Aguarda o usuário pressionar Enter antes de fechar o terminal.
+    }
+}
