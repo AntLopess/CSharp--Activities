d2eff67e1ea6309726943171675163003204414c
AntLope <lusdf47@gmail.com>
2026-08-27 10:55:19 -0300

    Add files via upload

---
+++ Fundamentos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace OperaçõesAritmeticas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 5;
+            int soma = a + b;
+            int produto = a * b;
+
+            Console.WriteLine("Soma: " + soma);
+            Console.WriteLine("Produto: " + produto);
+            Console.ReadLine();
+        }
+    }
+}
