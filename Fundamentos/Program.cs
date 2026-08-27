898ede3e241c6eca5babf90fe5df24d1f47cd765
AntLope <lusdf47@gmail.com>
2026-08-27 10:56:37 -0300

    Add files via upload

---
+++ Fundamentos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace EntradaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Olá, " + nome + "!");
+            Console.ReadLine();
+        }
+    }
+}
